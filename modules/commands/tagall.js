32c5dd2fa0eacd9fd92c88e6051f8b928eed4ce8
ledat123Q <113812595+ledat123Q@users.noreply.github.com>
2025-05-18 12:11:15 +0700

    tagall.js

--- modules/commands/tagall.js
+++ modules/commands/tagall.js
@@ -1,42 +1,27 @@
-module.exports = {
-  config: {
-    name: "tagall",
-    aliases: ["all", "everyone"],
-    permissions: [1, 2],
-    description: "Mention all members in group",
-    usage: "<text>",
-    cooldown: 10,
-    commandCategory: "group", // bắt buộc phải có nếu bot yêu cầu
-    credits: "XaviaTeam"
-  },
+module.exports.config = {
+  name: "tagall",
+  version: "1.0.0",
+  hasPermssion: 1,
+  credits: "XaviaTeam",
+  description: "Tag toàn bộ thành viên trong nhóm",
+  commandCategory: "group",
+  usages: "[Text]",
+  cooldowns: 5
+};
 
-  langData: {
-    "en_US": {
-      "tagall.defaultText": "@everyone"
-    },
-    "vi_VN": {
-      "tagall.defaultText": "@mọi người"
-    }
-  },
+module.exports.run = async function ({ api, event, args }) {
+  const botID = api.getCurrentUserID();
+  const allUser = event.participantIDs.filter(id => id !== botID && id !== event.senderID);
 
-  onCall({ message, args, getLang }) {
-    const { isGroup, senderID, participantIDs } = message;
-    if (!isGroup) return;
+  const text = args.join(" ") || "@mọi người";
+  const mentions = [];
 
-    const emptyChar = '\u200B';
-    const text = args.join(" ") || getLang("tagall.defaultText");
-    const mentions = participantIDs
-      .filter(e => e != global.botID && e != senderID)
-      .map((e, i) => ({ tag: text[i] || emptyChar, id: e }));
-
-    if (mentions.length > 0) {
-      message.reply({
-        body:
-          mentions.length > text.length
-            ? text + emptyChar.repeat(mentions.length - text.length)
-            : text,
-        mentions
-      });
-    }
+  for (let id of allUser) {
+    mentions.push({
+      tag: text,
+      id
+    });
   }
+
+  return api.sendMessage({ body: text, mentions }, event.threadID, event.messageID);
 };
